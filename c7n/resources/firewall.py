d116ecb26d04f880287992296468bea4ae270b84
Jerry Xu <jerryhxu@yahoo.com>
2024-03-07 00:25:51 -0500

    feat: add network firewall tagging and logging configuration

--- c7n/resources/firewall.py
+++ c7n/resources/firewall.py
@@ -5,7 +5,14 @@ from .aws import AWS
 from c7n.query import (
     QueryResourceManager, TypeInfo, DescribeSource, ConfigSource)
 from c7n.filters.vpc import VpcFilter, SubnetFilter
+from c7n.filters import ListItemFilter, FilterRegistry
+from c7n.actions import ActionRegistry, BaseAction
+from c7n.utils import local_session, type_schema
+from .aws import shape_validate
+from c7n.tags import RemoveTag, Tag, TagActionFilter, TagDelayedAction
 
+filters = FilterRegistry('aws.account.filters')
+actions = ActionRegistry('aws.account.actions')
 
 class FirewallDescribe(DescribeSource):
 
@@ -63,3 +70,162 @@ class FirewallVpcFilter(VpcFilter):
 class FirewallSubnetFilter(SubnetFilter):
 
     RelatedIdsExpression = 'SubnetMappings[].SubnetId'
+
+@NetworkFirewall.action_registry.register('tag')
+class TagNetworkFirewall(Tag):
+    """Create tags on Network Firewalls
+
+    :example:
+
+    .. code-block:: yaml
+
+        policies:
+            - name: network-firewall-tag
+              resource: aws.firewall
+              actions:
+                - type: tag
+                  key: test
+                  value: something
+    """
+    permissions = ('network-firewall:TagResource',)
+
+    def process_resource_set(self, client, resources, new_tags):
+        #tags = [{'key': item['Key'], 'value': item['Value']} for item in new_tags]
+        for r in resources:
+            client.tag_resource(ResourceArn=r["FirewallArn"], Tags=new_tags)
+
+
+@NetworkFirewall.action_registry.register('remove-tag')
+class RemoveNetworkFirewall(RemoveTag):
+    """Remove tags from a network firewall
+    :example:
+
+    .. code-block:: yaml
+
+        policies:
+            - name: network-firewall-remove-tag
+              resource: aws.firewall
+              actions:
+                - type: remove-tag
+                  tags: ["tag-key"]
+    """
+    permissions = ('network-firewall:UntagResource',)
+
+    def process_resource_set(self, client, resources, tags):
+        for r in resources:
+            client.untag_resource(ResourceArn=r['FirewallArn'], TagKeys=tags)
+
+
+NetworkFirewall.filter_registry.register('marked-for-op', TagActionFilter)
+@NetworkFirewall.action_registry.register('mark-for-op')
+class MarkNetworkFirewallForOp(TagDelayedAction):
+    """Mark network firewall for future actions
+
+    :example:
+
+    .. code-block:: yaml
+
+        policies:
+          - name: network-firewall-tag-mark
+            resource: aws.firewall
+            filters:
+              - "tag:delete": present
+            actions:
+              - type: mark-for-op
+                op: delete
+                days: 1
+    """
+
+@NetworkFirewall.filter_registry.register('logging')
+class NetworkFirewallLogging(ListItemFilter):
+    """Filter for network firewall to look at network firewall logging configuration
+
+    The schema to supply to the attrs follows the schema here:
+     https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/network-firewall/client/describe_logging_configuration.html
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: network-firewall-logging-configuration
+                resource: aws.network-firewall
+                filters:
+                #   - type: logging
+                #     attrs:
+                #       - LogType: ALERT
+                  - type: logging
+                    attrs:
+                      - LogType: FLOW
+    """
+    schema = type_schema(
+        'logging',
+        attrs={'$ref': '#/definitions/filters_common/list_item_attrs'},
+        count={'type': 'number'},
+        count_op={'$ref': '#/definitions/filters_common/comparison_operators'}
+    )
+    permissions = ('network-firewall:DescribeLoggingConfiguration',)
+    annotation_key = 'c7n:NetworkFirewall'
+
+    def get_item_values(self, resource):
+        client = local_session(self.manager.session_factory).client('network-firewall')
+        resource[self.annotation_key] = client \
+                .describe_logging_configuration(
+                    FirewallArn=resource['FirewallArn'],
+                    FirewallName=resource['FirewallName'])\
+                .get('LoggingConfiguration', {}).get('LogDestinationConfigs', [])
+
+        return resource.get(self.annotation_key)
+
+
+@NetworkFirewall.action_registry.register('delete')
+class DeleteNetworkFirewall(BaseAction):
+    """Delete a network firewall
+
+    :example:
+
+    .. code-block:: yaml
+
+        policies:
+          - name: network-firewall-delete
+            resource: aws.firewall
+            actions:
+              - type: delete
+    """
+    schema = type_schema('delete', )
+    permissions = ('network-firewall:DeleteFirewall',)
+
+    def process(self, resources):
+        client = local_session(self.manager.session_factory).client('network-firewall')
+        for r in resources:
+            try:
+              client.delete_firewall(
+                  FirewallName=r['FirewallName'],
+                  FirewallArn =r['FirewallArn']
+                  )
+            except client.exceptions.ResourceNotFoundException:
+              continue
+
+
+@NetworkFirewall.action_registry.register('update-delete-protection')
+class UpdateNetworkFirewall(BaseAction):
+    """Enable/disable network firewall delete protection."""
+
+    permissions = ('network-firewall:UpdateFirewallDeleteProtection',)
+
+    schema = type_schema(
+        'update-delete-protection',
+        state={'type': 'boolean'})
+
+    def process(self, resources):
+        client = local_session(self.manager.session_factory).client('network-firewall')
+        state = self.data.get('state', True)
+        for r in resources:
+            try:
+                client.update_firewall_delete_protection(
+                    FirewallName=r['FirewallName'],
+                    FirewallArn =r['FirewallArn'],
+                    DeleteProtection = state
+                    )
+            except client.exceptions.ResourceNotFoundException:
+                continue
--- tests/test_firewall.py
+++ tests/test_firewall.py
@@ -27,3 +27,150 @@ class NetworkFirewallTest(BaseTest):
         resources = p.run()
         self.assertEqual(len(resources), 1)
         assert resources[0]['FirewallName'] == 'unicron'
+
+    def test_firewall_tag_untag(self):
+        session_factory = self.replay_flight_data('test_firewall_tag_untag')
+        tag = {'env': 'dev'}
+        p = self.load_policy(
+            {
+                'name': 'firewall-tag-untag',
+                'resource': 'firewall',
+                'filters': [{
+                    'tag:owner': 'policy'
+                }],
+                'actions': [{
+                    'type': 'tag',
+                    'tags': tag
+                },
+                {
+                    'type': 'remove-tag',
+                    'tags': ['owner']
+                }]
+            },
+            session_factory=session_factory
+        )
+        resources = p.run()
+        self.assertEqual(1, len(resources))
+        client = session_factory().client("network-firewall")
+        firewall = client.describe_firewall(FirewallName=resources[0]['FirewallName'])
+        tags = firewall['Firewall'].get('Tags')
+        self.assertEqual(1, len(tags))
+        new_tag = {}
+        new_tag[tags[0]['Key']] = tags[0]['Value']
+        self.assertEqual(tag, new_tag)
+
+    def test_firewall_mark_for_op(self):
+        session_factory = self.replay_flight_data("test_firewall_mark_for_op")
+        p = self.load_policy(
+            {
+                "name": "firewall-mark",
+                "resource": "firewall",
+                "filters": [
+                    {"tag:owner": "policy"},
+                ],
+                "actions": [
+                    {
+                        "type": "mark-for-op",
+                        "tag": "custodian_cleanup",
+                        "op": "delete",
+                        "days": 1,
+                    }
+                ],
+            },
+            session_factory=session_factory
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+        p = self.load_policy(
+            {
+                "name": "firewall-marked",
+                "resource": "firewall",
+                "filters": [
+                    {
+                        "type": "marked-for-op",
+                        "tag": "custodian_cleanup",
+                        "op": "delete",
+                        "skew": 3,
+                    }
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+    def test_firewall_logging(self):
+        factory = self.record_flight_data('test_network_firewall_logging')
+        p = self.load_policy(
+            {
+                "name": "firewall-config",
+                "resource": "aws.firewall",
+                "filters": [
+                        {
+                            "type": "logging",
+                            "attrs": [
+                            {"LogType": "ALERT"},
+                            ]
+                        }
+                    ],
+                },
+            session_factory = factory,
+            config = {'region': 'us-east-1'}
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        assert resources[0]['FirewallName'] == 'test-firewall'
+
+    def test_delete_firewall(self):
+        session_factory = self.record_flight_data("test_delete_firewall")
+        p = self.load_policy(
+            {
+                "name": "delete-firewall",
+                "resource": "firewall",
+                "filters": [{"tag:owner": "policy"}],
+                "actions": [{"type": "delete"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(resources[0]["FirewallName"], "test-firewall")
+
+    # def test_force_delete_dynamodb_tables(self):
+    #     session_factory = self.replay_flight_data("test_force_delete_dynamodb_tables")
+    #     client = session_factory().client("dynamodb")
+    #     self.patch(DeleteTable, "executor_factory", MainThreadExecutor)
+    #     p = self.load_policy(
+    #         {
+    #             "name": "delete-empty-tables",
+    #             "resource": "dynamodb-table",
+    #             "filters": [{"TableName": "c7n-test"}],
+    #             "actions": [
+    #                 {
+    #                     "type": "delete",
+    #                     "force": True
+    #                 }
+    #             ],
+    #         },
+    #         session_factory=session_factory,
+    #     )
+    #     resources = p.run()
+    #     self.assertEqual(resources[0]["DeletionProtectionEnabled"], True)
+    #     table = client.describe_table(TableName="c7n-test")["Table"]
+    #     self.assertEqual(table.get('TableStatus'), 'DELETING')
+
+    # def test_update_tables(self):
+    #     session_factory = self.replay_flight_data("test_dynamodb_update_table")
+    #     client = session_factory().client("dynamodb")
+    #     p = self.load_policy(
+    #         {
+    #             "name": "update-empty-tables",
+    #             "resource": "dynamodb-table",
+    #             "actions": [{"type": "update", "BillingMode": "PAY_PER_REQUEST"}],
+    #         },
+    #         session_factory=session_factory,
+    #     )
+    #     resources = p.run()
+    #     assert resources[0]["TableName"] == "cc-testing-table"
+    #     t = client.describe_table(TableName="cc-testing-table")["Table"]
+    #     assert t["BillingModeSummary"]["BillingMode"] == "PAY_PER_REQUEST"
